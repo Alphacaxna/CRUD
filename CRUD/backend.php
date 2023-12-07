9caa281fa40a2e817c79841b40c0d139981e0776
Alphacaxna <119132805+Alphacaxna@users.noreply.github.com>
2023-12-07 09:10:49 -0600

    Update backend.php

--- CRUD/backend.php
+++ CRUD/backend.php
@@ -5,89 +5,87 @@ $username = "root";
 $password = "";
 $dbname = "crud";
 
-// Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Manejar la solicitud POST (Crear o Actualizar)
+$response = array();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $precio = $_POST["precio"];
 
-    // Utilizar consultas preparadas para prevenir inyección SQL
     $stmt = $conn->prepare("INSERT INTO productos (nombre, precio) VALUES (?, ?)");
     $stmt->bind_param("sd", $nombre, $precio);
 
     if ($stmt->execute()) {
-        echo json_encode(array("mensaje" => "Producto agregado exitosamente"));
+        $response["mensaje"] = "Producto agregado exitosamente";
     } else {
-        echo json_encode(array("mensaje" => "Error: " . $stmt->error));
+        $response["error"] = "Error al ejecutar la consulta: " . $stmt->error;
     }
 
     $stmt->close();
-}
-
-// Manejar la solicitud PUT (Actualizar)
-if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+} elseif ($_SERVER["REQUEST_METHOD"] == "PUT") {
     parse_str(file_get_contents("php://input"), $_PUT);
     $id = $_PUT["id"];
     $nombre = $_PUT["nombre"];
     $precio = $_PUT["precio"];
 
-    // Utilizar consultas preparadas para prevenir inyección SQL
     $stmt = $conn->prepare("UPDATE productos SET nombre = ?, precio = ? WHERE id = ?");
     $stmt->bind_param("sdi", $nombre, $precio, $id);
 
     if ($stmt->execute()) {
-        echo json_encode(array("mensaje" => "Producto actualizado exitosamente"));
+        $response["mensaje"] = "Producto actualizado exitosamente";
     } else {
-        echo json_encode(array("mensaje" => "Error: " . $stmt->error));
+        $response["error"] = "Error al ejecutar la consulta: " . $stmt->error;
     }
 
     $stmt->close();
-}
-
-// Manejar la solicitud DELETE
-if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+} elseif ($_SERVER["REQUEST_METHOD"] == "DELETE") {
     $id = $_GET["id"];
 
-    // Utilizar consultas preparadas para prevenir inyección SQL
     $stmt = $conn->prepare("DELETE FROM productos WHERE id = ?");
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
-        echo json_encode(array("mensaje" => "Producto eliminado exitosamente"));
+        $response["mensaje"] = "Producto eliminado exitosamente";
     } else {
-        echo json_encode(array("mensaje" => "Error: " . $stmt->error));
+        $response["error"] = "Error al ejecutar la consulta: " . $stmt->error;
     }
 
     $stmt->close();
-}
-
-// Manejar la solicitud GET (Leer)
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
     $nombreBusqueda = isset($_GET["nombreBusqueda"]) ? $_GET["nombreBusqueda"] : "";
     
     $consulta = "SELECT * FROM productos";
     
     if (!empty($nombreBusqueda)) {
-        // Agregar condición de búsqueda por nombre
         $consulta .= " WHERE nombre LIKE '%$nombreBusqueda%'";
     }
 
     $result = $conn->query($consulta);
-    $data = array();
 
-    while ($fila = $result->fetch_assoc()) {
-        $data[] = $fila;
+    if ($result) {
+        $data = array();
+
+        while ($fila = $result->fetch_assoc()) {
+            $data[] = $fila;
+        }
+
+        $response["data"] = $data;
+    } else {
+        $response["error"] = "Error al ejecutar la consulta: " . $conn->error;
     }
-
-    echo json_encode($data);
 }
 
+// Añade el encabezado de respuesta para indicar que se envía JSON
+header('Content-Type: application/json');
+
+// Imprime la respuesta JSON
+echo json_encode($response);
+
 $conn->close();
+
 ?>
